9c0ed39543ebd0566eb99cafbac02e31b092ffbf
Anurag Rawat <anuragrawat88393@gmail.com>
2024-06-02 23:39:41 +0530

    first commit

--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -1,67 +1,82 @@
-import React, { useContext, useRef } from 'react'
-import './login.css'
+import React, { useContext, useRef } from "react";
+import "./login.css";
 
-import { AuthContext } from '../../../context/authContext';
-import { CircularProgress } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { loginCall } from '../../../apiCalls';
+import { AuthContext } from "../../../context/authContext";
+import { CircularProgress } from "@mui/material";
+import { Link } from "react-router-dom";
+import { loginCall } from "../../../apiCalls";
 
 function Login() {
-  
   const email = useRef();
   const password = useRef();
-  const {user , isFetching, login,dispatch} = useContext(AuthContext);
-  
-  const handleClick = (e) =>{
+  const { user, isFetching, login, dispatch } = useContext(AuthContext);
+
+  const handleClick = (e) => {
     e.preventDefault();
-      // login({ email : email.current.value, password : password.current.value})
-      loginCall(
-        { email: email.current.value, password: password.current.value },
-        dispatch
-      );
-  }
+    // login({ email : email.current.value, password : password.current.value})
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
 
-  console.log(user)
+  console.log(user);
   return (
-    <div className='loginContainer'>
-      
-           <div className="login">
-             <div className="imgDiv">
+    <div className="loginContainer">
+      <div className="loginLogo">
+        <img src="public/assets/logo.png" alt="" />
+      </div>
+      <div className="login">
+        {/* <div className="imgDiv">
                  <img className='loginImg' src="/assets/person/login.jpg" />
-             </div>
-              <div className="loginLeft">
-              <div className="login-form">
-             
-              <form action="" className="form" onSubmit={handleClick}>
-                  <div className="input-group">
-                  <div className="form-input">
-                      <label htmlFor="email">Email</label>
-                      <input className='emailInput' type="email" id='email' required ref={email} placeholder="Enter email"/>
-                   </div>
-                      <div className="form-input">
-                          <label htmlFor="password">Password</label>
-                          <input className='passwordInput' type="password" ref={password} minLength={6} required id='password' placeholder="Enter first password"/>
-                     </div>
-
-                      <div className="action-btn">
-                          <button  className="btn-login" disabled={isFetching}>{isFetching ? <CircularProgress size="15px" /> : "Log In"}</button>
-                      </div>
-                      <div className="or">or</div>
-                      <div className="action-btn">
-                          <Link to='/register'><button className="btn-login">sign-up</button></Link>
-                      </div>
-                 </div>
-              </form>
-          
- 
-            </div>
+             </div> */}
+        <div className="loginLeft">
+          <div className="login-form">
+            <form action="" className="form" onSubmit={handleClick}>
+              <div className="input-group">
+                <div className="form-input">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="emailInput"
+                    type="email"
+                    id="email"
+                    required
+                    ref={email}
+                    placeholder="Enter email"
+                  />
+                </div>
+                <div className="form-input">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    className="passwordInput"
+                    type="password"
+                    ref={password}
+                    minLength={6}
+                    required
+                    id="password"
+                    placeholder="Enter first password"
+                  />
+                </div>
               </div>
-
-           </div>
-           
-
+              <div className="form-btns">
+              <div className="action-btn">
+                  <button className="btn-login" disabled={isFetching}>
+                    {isFetching ? <CircularProgress size="15px" /> : "Log In"}
+                  </button>
+                </div>
+                <div className="or">or</div>
+                <div className="action-btn">
+                  <Link to="/register">
+                    <button className="btn-login">sign-up</button>
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
